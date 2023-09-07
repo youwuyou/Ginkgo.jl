c433ffc31c5d78160c51240717ebaff8a10a0765
Documenter.jl <documenter@juliadocs.github.io>
2023-09-07 18:46:11 +0000

    build based on 1aec7e7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ginkgo","category":"page"},{"location":"#Ginkgo","page":"Home","title":"Ginkgo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ginkgo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ginkgo]","category":"page"}]
+}
