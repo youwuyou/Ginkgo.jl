ca8139350885c8868c64d8b9ac7c29d12e34e57f
You Wu <youwuyou@student.ethz.ch>
2024-02-29 16:25:49 +0100

    improved conversion between ginkgo dense matrix and julia vector #27

--- src/matrix/Dense.jl
+++ src/matrix/Dense.jl
@@ -12,13 +12,13 @@ A type for representing dense matrix and vectors. Alias for `gko_matrix_dense_el
 
 ```julia-repl
 # Creating uninitialized vector of length 2, represented as a 2x1 dense matrix
-julia> dim = (2,1); vec1 = GkoDense{Float32}(dim, exec)
+julia> dim = (2,1); vec1 = GkoDense{Float32}(undef, dim, exec)
 
-# Passing a tuple
-julia> vec2 = GkoDense{Float32}((2, 1), exec)
+# Passing dimensions as a tuple of numbers
+julia> vec2 = GkoDense{Float32}(undef, (2, 1), exec)
 
-# Passing numbers
-julia> vec3 = GkoDense{Float32}(2, 1, exec)
+# Passing dimensions as numbers
+julia> vec3 = GkoDense{Float32}(undef, 2, 1, exec)
 
 # Creating initialized dense vector or matrix via reading from a `.mtx` file
 julia> b = GkoDense{Float32}("b.mtx", exec)
@@ -33,15 +33,13 @@ mutable struct GkoDense{T} <: AbstractMatrix{T}
 
     ############################# CONSTRUCTOR ####################################
     # Constructors for matrix with uninitialized values
-    function GkoDense{T}(m::Integer, n::Integer, executor::GkoExecutor = EXECUTOR[]) where T
-        # @warn "Constructing $(m) x $(n) matrix with uninitialized values. Displayed values may not be meaningful. It's advisable to initialize the matrix before usage."
+    function GkoDense{T}(::UndefInitializer, m::Integer, n::Integer, executor::GkoExecutor = EXECUTOR[]) where T
         function_name = Symbol("ginkgo_matrix_dense_", gko_type(T), "_create")
         ptr = eval(:($API.$function_name($(executor.ptr), ($m,$n))))
         finalizer(delete_dense_matrix, new{T}(ptr, executor))
     end
 
-    function GkoDense{T}(m::Tuple{Integer, Integer}, executor::GkoExecutor = EXECUTOR[]) where T
-        # @warn "Constructing $(m[1]) x $(m[2]) matrix with uninitialized values. Displayed values may not be meaningful. It's advisable to initialize the matrix before usage."
+    function GkoDense{T}(::UndefInitializer, m::Tuple{Integer, Integer}, executor::GkoExecutor = EXECUTOR[]) where T
         function_name = Symbol("ginkgo_matrix_dense_", gko_type(T), "_create")
         ptr = eval(:($API.$function_name($(executor.ptr), $m)))
         finalizer(delete_dense_matrix, new{T}(ptr, executor))
@@ -55,6 +53,35 @@ mutable struct GkoDense{T} <: AbstractMatrix{T}
         finalizer(delete_dense_matrix, new{T}(ptr, executor))
     end
 
+
+    # TODO: any ways to avoid the copying of data?
+    """
+        GkoDense(values::Vector{T}, executor::GkoExecutor = EXECUTOR[]) where T
+
+    Create a dense vector object, which is internally a nx1 matrix. We assume `stride == 1`.
+
+    # Arguments
+    - `n::Integer`: The length of the vector to be created.
+    - `values::Vector{T}`: A one-dimensional array containing the elements of the vector. The type `T` specifies `eltype(values)`.
+    - `executor::GkoExecutor`: The executor where the dense vector will be created. Defaults to `EXECUTOR[]`, which enables the implicit executor usage.
+
+    # Returns
+    A `GkoDense` object representing the dense vector of length n, which is also a nx1 matrix.
+
+    # Example
+    ```julia
+    exec = create(:omp)
+    values = rand(Float64, 20) # Generate random values
+    vector = GkoDense(values, exec) # Create the GkoDense vector (nx1 matrix)
+    ```
+    """
+    function GkoDense(values::Vector{T}, executor::GkoExecutor = EXECUTOR[]) where T
+        function_name = Symbol("ginkgo_matrix_dense_", gko_type(T), "_create_view")
+        ptr = eval(:($API.$function_name($(executor.ptr), ($length($values), 1), $values, 1)))
+        finalizer(delete_dense_matrix, new{T}(ptr, executor))
+    end
+
+    # TODO: add the support for dense matrix
 
     ############################# DESTRUCTOR ####################################
     function delete_dense_matrix(mat::GkoDense{T}) where T
@@ -82,23 +109,34 @@ function Base.getindex(mat::GkoDense{T}, m::Integer, n::Integer) where T
     return (eval(:($API.$function_name($mat.ptr, $(m-1), $(n-1)))))
 end
 
-
 """
     number(val::Number, exec::GkoExecutor = EXECUTOR[])
 
 Initialize a 1x1 matrix representing a number with the provided value `val`
 """
-function number(val::Number, exec::GkoExecutor = EXECUTOR[])
-    T = typeof(val)
-    mat = GkoDense{T}((1,1), exec)
-    fill!(mat, val)
-    return mat
-end
+number(val::Number, exec::GkoExecutor = EXECUTOR[]) = GkoDense([val], exec)
 
 # function Base.ones(size::Int)
 # end
 
 # Array operations
+
+
+# TODO: get the underlying value stored in GkoDense
+# FIXME: how about using it directly in the type conversion?
+# 1. we obtain the pointer to the array so no copy happens
+# 2. do I get the value arrays on the device?? or it is just value array copy on host?
+
+# function ginkgo_matrix_dense_f64_get_values(mat_st_ptr)
+#     ccall((:ginkgo_matrix_dense_f64_get_values, libginkgo), Ptr{Cdouble}, (gko_matrix_dense_f64,), mat_st_ptr)
+# end
+
+# function ginkgo_matrix_dense_f64_get_const_values(mat_st_ptr)
+#     ccall((:ginkgo_matrix_dense_f64_get_const_values, libginkgo), Ptr{Cdouble}, (gko_matrix_dense_f64,), mat_st_ptr)
+# end
+
+
+
 """
     Base.fill!(mat::GkoDense{T}, val::G) where {T, G}
 
@@ -161,21 +199,21 @@ function norm2!(from::GkoDense{T}, to::GkoDense{G}) where {T, G}
 end
 
 
-"""
-    mtx_buffer_str(mat::GkoDense{T}) where T
+# """
+#     mtx_buffer_str(mat::GkoDense{T}) where T
 
-Intermediate step that calls `gko::write` within C level wrapper. Allocates memory temporarily
-and returns a string pointer in C, then we utilize an IOBuffer to obtain a copy of the allocated cstring
-in Julia. In the end we deallocate the C string and return the buffered copy.
-"""
-function mtx_buffer_str(mat::GkoDense{T}) where T
-    buf = IOBuffer()
-    cstr_ptr = API.ginkgo_matrix_dense_f32_write_mtx(mat.ptr)
-    write(buf, unsafe_string(cstr_ptr))
-    API.c_char_ptr_free(cstr_ptr)
-    return String(take!(buf))
-end
+# Intermediate step that calls `gko::write` within C level wrapper. Allocates memory temporarily
+# and returns a string pointer in C, then we utilize an IOBuffer to obtain a copy of the allocated cstring
+# in Julia. In the end we deallocate the C string and return the buffered copy.
+# """
+# function mtx_buffer_str(mat::GkoDense{T}) where T
+#     buf = IOBuffer()
+#     cstr_ptr = API.ginkgo_matrix_dense_f32_write_mtx(mat.ptr)
+#     write(buf, unsafe_string(cstr_ptr))
+#     API.c_char_ptr_free(cstr_ptr)
+#     return String(take!(buf))
+# end
 
 # Display
-# FIXME: way faster than the usual one
-Base.show(io::IO, ::MIME"text/plain", mat::GkoDense{T}) where T = print(io, mtx_buffer_str(mat))
+# FIXME: way faster than the usual one?
+# Base.show(io::IO, ::MIME"text/plain", mat::GkoDense{T}) where T = print(io, mtx_buffer_str(mat))
